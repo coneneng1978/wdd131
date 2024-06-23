c5575defe7814a8d03c3e409ef948a6f2c32e9c1
coneneng1978 <155893749+coneneng1978@users.noreply.github.com>
2024-06-23 11:08:58 +0800

    update_4

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,22 +1,22 @@
-// temples.js
-
-// getdates.js
-
 // Get the current year and set it to the span with id "currentyear"
-const currentYearSpan = document.getElementById('currentyear');
-const currentYear = new Date().getFullYear();
-currentYearSpan.textContent = currentYear;
+function setCurrentYear() {
+    const currentYearSpan = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    currentYearSpan.textContent = currentYear;
+}
 
 // Get the last modified date and set it to the paragraph with id "lastModified"
-const lastModifiedParagraph = document.getElementById('lastModified');
-const lastModified = document.lastModified;
-lastModifiedParagraph.textContent = `Last Modified: ${lastModified}`;
+function setLastModified() {
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    const lastModified = document.lastModified;
+    lastModifiedParagraph.textContent = `Last Modified: ${lastModified}`;
+}
 
 // Toggle mobile navigation menu
-const menuToggle = document.querySelector('.menu-toggle');
+const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav ul');
 
-menuToggle.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
     nav.classList.toggle('show');
 });
 
